89fa9ec746d49eb84383274d8b9a91990c40a17d
JoeJiJa <168629159+JoeJiJa@users.noreply.github.com>
2024-05-01 16:03:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+DrJay = Client(
+      name="DrAstrobot",
+      api_id=API_ID,
+      api_hash=API_HASH,
+      bot_token=BOT_TOKEN
+)
+
+
+print("Bot started")
+
+DrJay.run()
